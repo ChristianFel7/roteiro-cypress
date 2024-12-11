997db2c06baa374d94608d1808021f38f1164095
Christian <75313987+ChristianFel7@users.noreply.github.com>
2024-12-10 23:58:10 -0300

    Update spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -46,7 +46,7 @@ describe('TODOMvc App', () => {
       .first()
       .click();
 
-    cy.get('[data-cy=filter-active-link')
+    cy.get('[data-cy=filter-active-link]') // Corrigido: faltava o ']'
       .click();
     cy.get('[data-cy=todos-list]')
       .children()
@@ -54,7 +54,7 @@ describe('TODOMvc App', () => {
       .first()
       .should('have.text', 'Prova de ES');
 
-    cy.get('[data-cy=filter-completed-link')
+    cy.get('[data-cy=filter-completed-link]')
       .click();
     cy.get('[data-cy=todos-list]')
       .children()
@@ -62,10 +62,52 @@ describe('TODOMvc App', () => {
       .first()
       .should('have.text', 'TP2 de ES');
 
-    cy.get('[data-cy=filter-all-link')
+    cy.get('[data-cy=filter-all-link]')
       .click();
     cy.get('[data-cy=todos-list]')
       .children()
       .should('have.length', 2);
   });
+
+  it('Marca todas as tarefas como completas e depois as limpa', () => {
+    cy.visit('');
+    cy.get('[data-cy=todo-input]')
+      .type('Tarefa 1{enter}')
+      .type('Tarefa 2{enter}');
+
+    cy.get('[data-cy=toggle-all-checkbox]').click();
+    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]:checked').should('have.length', 2);
+
+    cy.get('[data-cy=clear-completed-btn]').click();
+    cy.get('[data-cy=todos-list]').children().should('have.length', 0);
+  });
+
+
+  it('Edita uma tarefa existente', () => {
+    cy.visit('');
+    cy.get('[data-cy=todo-input]')
+      .type('Tarefa Original{enter}');
+
+    cy.get('[data-cy=todos-list] > li').dblclick();  // Editar a tarefa
+    cy.get('[data-cy=todos-list] > li input.edit').clear().type('Tarefa Editada{enter}');
+
+    cy.get('[data-cy=todos-list] > li').should('have.text', 'Tarefa Editada');
+  });
+
+  it('Verifica a contagem de itens restantes', () => {
+    cy.visit('');
+    cy.get('[data-cy=todo-input]')
+      .type('Tarefa 1{enter}')
+      .type('Tarefa 2{enter}');
+
+    cy.get('[data-cy=todo-count]').should('contain', '2');
+
+    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]').first().click();
+    cy.get('[data-cy=todo-count]').should('contain', '1');
+
+  });
+
+
+
+
 });
